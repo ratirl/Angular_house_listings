f1601f772cdb007f9710ec840f1f76217a3f9732
ratirl <sjokz@hotmail.com>
2024-07-29 19:49:26 +0200

    added routing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+// the two below imports are to enable routing in angular for this project
+import {provideRouter} from '@angular/router';
+import routeConfig from './app/routes';
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [ provideRouter(routeConfig)],
+}).catch((err) => console.error(err));
