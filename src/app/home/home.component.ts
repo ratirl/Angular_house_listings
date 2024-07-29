731ef59445ab62214f9a3ee487fb0951d8bb0b42
ratirl <sjokz@hotmail.com>
2024-07-29 13:29:51 +0200

    housing location component added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {HousingLocationComponent} from '../housing-location/housing-location.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
@@ -11,7 +12,10 @@ import {CommonModule} from '@angular/common';
         <button class="primary" type="button">Search</button>
       </form>
     </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
   `,
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {} 
