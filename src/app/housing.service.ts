6bb649473a8a36466aede7276c708760ad56a884
ratirl <sjokz@hotmail.com>
2024-07-30 22:23:29 +0200

    bugfix details page not working cuz id in json wasnt a string

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -11,6 +11,8 @@ export class HousingService {
   }
   async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
     const data = await fetch(`${this.url}/${id}`);
+    console.log("id is :" + id);
+    console.log(`The type of id is: ${typeof id}`); // Log the type of id
     return (await data.json()) ?? {};
   }
   submitApplication(firstName: string, lastName: string, email: string) {
